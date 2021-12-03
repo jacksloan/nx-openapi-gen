198d4a500453be1533cd04617a23282a03a6109a
jacksloan <jbsloan10@gmail.com>
2021-12-03 13:48:05 -0600

    initial commit

---
+++ e2e/nest-gen-e2e/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'nest-gen-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/e2e/nest-gen-e2e',
+};
---
+++ packages/nest-gen/src/generators/nest-gen/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NestGenGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
